6e5a62fc888a1946ed1f47b2cd9575d389661814
Anas-Saifi7 <123534963+Anas-Saifi7@users.noreply.github.com>
2023-05-14 16:05:25 +0530

    Add files via upload

---
+++ equal and not equal .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int n1,n2;
+	printf("enter the n1 ");
+	scanf("%d",&n1);
+	
+		printf("enter the n2");
+	scanf("%d",&n2);
+	if(n1==n2){
+		printf("equal");
+	}
+	else{
+		printf("not equal");
+	}
+	return 0;
+
+}
